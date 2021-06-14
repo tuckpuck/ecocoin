1bf7825bb608617e690e66fe722288779f7e63cd
tuckpuck <tuckertriggs@gmail.com>
2021-06-14 13:29:57 -0600

    broadcast state of blockchain to nodes

--- block.ts
+++ block.ts
@@ -116,3 +116,31 @@ const replaceChain = (newBlocks: Block[]) => {
     console.log("Received blockchain invalid");
   }
 };
+
+const initHttpServer = (myHttpPort: number) => {
+  const app = express();
+  app.use(bodyParser.json());
+
+  app.get("/blocks", (req, res) => {
+    res.send(getBlockchain());
+  });
+  app.post("/mineBlock", (req, res) => {
+    const newBlock: Block = generateNextBlock(req.body.data);
+    res.send(newBlock);
+  });
+  app.get("/peers", (req, res) => {
+    res.send(
+      getSockets().map(
+        (s: any) => s._socket.remoteAddress + ":" + s._socket.remotePort
+      )
+    );
+  });
+  app.post("/addPeer", (req, res) => {
+    connectToPeers(req.body.peer);
+    res.send();
+  });
+
+  app.listen(myHttpPort, () => {
+    console.log("Listening http on port: " + myHttpPort);
+  });
+};
