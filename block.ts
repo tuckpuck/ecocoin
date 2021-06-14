3ac62ef90b3fac5bb481a9d0d3b6f552008b350f
tuckpuck <tuckertriggs@gmail.com>
2021-06-14 13:19:37 -0600

    hard code genesis block

--- block.ts
+++ block.ts
@@ -26,3 +26,11 @@ const calculateHash = (
   data: string
 ): string =>
   CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+const genesisBlock: Block = new Block(
+  0,
+  "816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7",
+  null,
+  1465154705,
+  "my genesis block!!"
+);
